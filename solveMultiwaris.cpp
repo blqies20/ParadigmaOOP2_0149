c61c50182598f23ac256f452777d857720541999
blqies20 <balqis1maharani@gmail.com>
2025-06-03 11:10:57 +0700

    hanya beda "virtual" dibandingakan dengan multiwaris

---
+++ solveMultiwaris.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public:
+        int umur;
+
+        orang(int pUmur) :
+        umur(pUmur) {
+            cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+        }
+};
+
+class pekerja : public orang {
+    public:
+        pekerja(int pUmur) :
+            orang(pUmur) { 
+                cout << "Pekerja dibuat\n" << endl;
+        }
+};
+
+class pelajar : public orang {
+    public:
+        pelajar(int pUmur) :
+            orang(pUmur) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+};
+
+class Soobin : public pekerja, public pelajar {
+    public:
+        Soobin(int pUmur) :
+            pekerja(pUmur),
+            pelajar(pUmur)
+            {
+                cout << "Soobin dibuat\n" << endl;
+            }
+};
+
+int main () {
+    Soobin a(22);
+
+    return 0;
+}
