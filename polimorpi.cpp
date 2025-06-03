5f60e70e06a903b160d9981cc63bacb151dece6b
blqies20 <balqis1maharani@gmail.com>
2025-06-03 10:01:46 +0700

    first commit

---
+++ polimorpi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+private:
+    /* data */
+public:
+    seseorang(/* args */);
+    ~seseorang();
+};
+
+seseorang::seseorang(/* args */)
+{
+}
+
+seseorang::~seseorang()
+{
+}
