463d9f5675ece2da54e80f972c7fe742eea87c8d
blqies20 <balqis1maharani@gmail.com>
2025-06-03 10:24:58 +0700

    membuat class dengan virtual dari class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -10,3 +10,19 @@ class orang {
             cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
         }
 };
+
+class pekerja : virtual public orang {
+    public:
+        pekerja(int pUmur) :
+            orang(pUmur) { 
+                cout << "Pekerja dibuat\n" << endl;
+        }
+};
+
+class pelajar : virtual public orang {
+    public:
+        pelajar(int pUmur) :
+            orang(pUmur) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+};
